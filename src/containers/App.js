47b0ebb253f80fb23f5a2679dd77bd304734dbba
florianbar <florianbar01@gmail.com>
2020-02-14 19:44:14 +0200

    Changed App component to class based

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList/CardList'
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <CardList />
-    </div>
-  );
+class App extends Component () {
+  render() {
+    return (
+      <div className="App">
+        <CardList />
+      </div>
+    );
+  }
 }
 
 export default App;
